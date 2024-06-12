e6f82d6dc916fb68a2b9078ad36dae4f1ea728e5
Diehlgit <arthurdiehlbarroso@gmail.com>
2024-06-12 11:44:24 -0300

    Criada view de enviar templates

--- camaar/config/routes.rb
+++ camaar/config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'home/assessment-id'
   get 'home/templates'
   get 'home/edit-template'
+  get 'home/send-template'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
